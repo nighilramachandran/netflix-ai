1f3f9f13351484df3e3d0fb8f4fa1eea050301c8
Nighil <nighilramm@gmail.com>
2024-12-31 20:25:24 +0400

    remove unused

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./widgets/layouts";
 import { ROUTES } from "./utils/constants/Routes";
 import AuthLayout from "./widgets/layouts/auth";
