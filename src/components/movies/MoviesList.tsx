5a3c119d39aac1a763bba3b0b43c6bc6baebba36
nighilramachandran <nighilramm@gmail.com>
2025-01-02 12:39:43 +0400

    remove browser error for scroll thumbkit

--- src/components/movies/MoviesList.tsx
+++ src/components/movies/MoviesList.tsx
@@ -24,7 +24,7 @@ const StyledStack = styled(Stack)(() => ({
   "&::-webkit-scrollbar": {
     display: "none",
   },
-  "-ms-overflow-style": "none",
-  "scrollbar-width": "none",
+  msOverflowStyle: "none",
+  scrollbarWidth: "none",
 }));
 export default MoviesList;
