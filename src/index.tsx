9882dd989ed13daf714981d296abba04732f30ae
nighilramachandran <nighilramm@gmail.com>
2025-01-10 15:33:58 +0400

    snackbar config

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import PrivateRoutes from "./Routes";
 import MotionLazyContainer from "./components/animate/MotionLazyContainer";
 import { Provider } from "react-redux";
 import store from "./redux/Store";
+import { SnackbarProvider } from "notistack";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,8 +16,15 @@ root.render(
   <Provider store={store}>
     <MotionLazyContainer>
       <ThemeProviders>
-        <CssBaseline enableColorScheme />
-        <PrivateRoutes />
+        <SnackbarProvider
+          maxSnack={2}
+          data-testid="toastid"
+          autoHideDuration={3000}
+          anchorOrigin={{ horizontal: "center", vertical: "top" }}
+        >
+          <CssBaseline enableColorScheme />
+          <PrivateRoutes />
+        </SnackbarProvider>
       </ThemeProviders>
     </MotionLazyContainer>
   </Provider>
