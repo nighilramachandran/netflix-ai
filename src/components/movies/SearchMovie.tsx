06cd24dd208c702176e5ff7f95538d4290b7f60d
nighilramachandran <nighilramm@gmail.com>
2025-01-10 15:19:04 +0400

    input focus after the animation complete

--- src/components/movies/SearchMovie.tsx
+++ src/components/movies/SearchMovie.tsx
@@ -35,15 +35,22 @@ const SearchMovie: React.FC<SearchMovieInputProps> = ({
   status,
   inputValue,
 }) => {
-  const searchKeyRef = useRef<HTMLButtonElement | null>(null);
+  const searchButtonRef = useRef<HTMLButtonElement | null>(null);
+  const inputBaseRef = useRef<HTMLElement | null>(null);
 
   const handleKeyPress = (
     e: React.KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>
   ) => {
     if (e.key === "Enter") {
-      if (searchKeyRef.current) {
-        searchKeyRef.current.click();
+      if (searchButtonRef.current) {
+        searchButtonRef.current.click();
       }
+    }
+  };
+
+  const handleAnimationComplete = () => {
+    if (inputBaseRef.current) {
+      inputBaseRef.current.focus();
     }
   };
 
@@ -53,6 +60,7 @@ const SearchMovie: React.FC<SearchMovieInputProps> = ({
       variants={searchMovieVariant}
       initial="hidden"
       animate={open ? "animate" : "exit"}
+      onAnimationComplete={handleAnimationComplete}
       sx={{
         flexWrap: "wrap",
         gap: "10px",
@@ -67,13 +75,14 @@ const SearchMovie: React.FC<SearchMovieInputProps> = ({
         onChange={inputChange}
         value={inputValue}
         onKeyDown={handleKeyPress}
+        inputRef={inputBaseRef}
       />
 
       <LoadingButton
         variant="contained"
         loading={status === "loading"}
         onClick={handleSearch}
-        ref={searchKeyRef}
+        ref={searchButtonRef}
         sx={{ width: "150px", height: "45px" }}
       >
         Search
