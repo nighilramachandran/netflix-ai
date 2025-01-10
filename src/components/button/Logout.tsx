64cf187e197d29c70c8d4d37b093e5e46c88ad42
nighilramachandran <nighilramm@gmail.com>
2025-01-10 17:02:01 +0400

    responsive logout button

--- src/components/button/Logout.tsx
+++ src/components/button/Logout.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { Button } from "@mui/material";
+import { Button, styled } from "@mui/material";
 import { useAppDispatch } from "../../redux/hooks";
 import { LogoutUserAsyncFunc } from "../../redux/auth";
+import useResponsive from "../../utils/hooks/useResponsive";
+import { m } from "framer-motion";
 
 const Logout: React.FC = () => {
   const disptach = useAppDispatch();
+
   const handleLogout = async () => {
     disptach(LogoutUserAsyncFunc());
   };
 
+  const downMd = useResponsive("down", "md");
+
   return (
-    <Button
+    <StyledAnimatedButton
       onClick={() => handleLogout()}
       variant="contained"
-      endIcon={<LogoutIcon />}
+      sx={{ gap: 1 }}
+      layout
     >
-      Logout
-    </Button>
+      {!downMd ? "Logout" : ""}
+      <LogoutIcon />
+    </StyledAnimatedButton>
   );
 };
+
+const StyledAnimatedButton = styled(m(Button))(() => ({}));
 
 export default Logout;
